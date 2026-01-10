61bfa6b8d2e6b61b9a577915922723b20b157853
dvsalmah <dealovasalmah12@gmail.com>
2026-01-10 22:29:19 +0700

    refactor: move sections into components directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-// HAPUS IMPORT 'BrowserRouter', Cukup Routes & Route saja
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Hero from './page/Hero';
-import AboutMe from './page/AboutMe';
-import Skills from './page/Skills';
-import Experiences from './page/Experiences';
-import Projects from './page/Projects-section';
-import ProjectDetail from './page/ProjectsDetail-section';
-import Contact from './page/Contact';
+import Hero from './sections/hero-section';
+import AboutMe from './sections/about-section';
+import Skills from './sections/skill-section';
+import Experiences from './sections/experience-section';
+import Projects from './sections/project-section';
+import ProjectDetail from './sections/project-detail';
+import Contact from './sections/contact-section';
 import Footer from './components/layout/Footer';
 
 function App() {
