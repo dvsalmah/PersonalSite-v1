42a1b4951299dd7a7f75542aa7d0c2913ce23d17
dvsalmah <dealovasalmah12@gmail.com>
2026-01-11 17:49:00 +0700

    feat: update downloadCV and refine hover style

--- src/components/sections/hero-section.jsx
+++ src/components/sections/hero-section.jsx
@@ -37,13 +37,15 @@ const Hero = () => {
   <a 
     href="#contact" 
     className="
-      inline-block rounded-full bg-[#F1A7C6] px-6 md:px-6 lg:px-8 py-2.5 md:py-3 text-xs lg:text-sm font-bold text-slate-900 shadow-[0_0_20px_rgba(241,167,198,0.3)] transition transform hover:bg-rose-300 hover:-translate-y-1 whitespace-nowrap"
+      inline-block rounded-full bg-[#F1A7C6] px-6 md:px-6 lg:px-8 py-2.5 md:py-3 text-xs lg:text-sm font-bold text-slate-900 shadow-[0_0_20px_rgba(241,167,198,0.3)] transition transform hover:bg-[#F1A7C6]/90 hover:-translate-y-1 whitespace-nowrap"
   >
     Get in Touch
   </a>
 
   <a 
-    href="#contact" 
+    href="https://docs.google.com/document/d/1sScghwcyFPmIeSGEEkkpL6tSK1l1PIlZ95X3KOnt3lc/edit?usp=sharing" 
+    target='_blank'
+    rel='noopener noreferrer'
     className="
       inline-block rounded-full border border-slate-600 px-6 md:px-6 lg:px-8 py-2.5 md:py-3 text-xs lg:text-sm font-semibold text-slate-300 hover:bg-slate-800 transition transform hover:-translate-y-1 whitespace-nowrap"
   >
