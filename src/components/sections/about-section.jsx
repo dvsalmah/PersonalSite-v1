bd22881f1ad3155c88bde1abed1a270b5c4ba5a6
dvsalmah <dealovasalmah12@gmail.com>
2026-01-11 17:50:41 +0700

    feat: add photo to stack component

--- src/components/sections/about-section.jsx
+++ src/components/sections/about-section.jsx
@@ -7,6 +7,7 @@ const AboutMe = () => {
     {id:2, src:"/assets/slide2.webp", alt:"Photo 2"},
     {id:3, src:"/assets/slide3.webp", alt:"Photo 3"},
     {id:4, src:"/assets/slide4.webp", alt:"Photo 4"},
+    {id:5, src:"/assets/slide5.webp", alt:"Photo 5"},
   ];
 
   const stackCards = photos.map(photo => (
